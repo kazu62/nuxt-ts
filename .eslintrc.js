f68a0d7f3eac7b9e7411a6d28c3eaccce8045584
KazuhisaOno <k.a-z.ono@betelgeuse-corp.com>
2019-04-25 07:43:02 +0900

    ts

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
+  },
+  extends: [
+    '@nuxtjs',
+    "airbnb",
+    "plugin:vue/recommended",
+    'plugin:prettier/recommended'
+  ],
+  plugins: [
+    'prettier',
+    '@typescript-eslint'
+  ],
+  // add your custom rules here
+  rules: {
+    'nuxt/no-cjs-in-config': 'off',
+    'no-unused-vars': 'off',
+    'no-unused-expressions': 'off'
+  }
+}
